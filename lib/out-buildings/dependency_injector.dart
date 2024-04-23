745b8aef89fb879b32f4559290f50aff4cf5e451
mahmoudmourad <mahmoudmourad1998@gmail.com>
2024-04-23 18:35:29 +0300

    adding dependencies to injector.
    (cherry picked from commit c1c49688a638ae9fd0146d20281a34e243054502)

--- lib/out-buildings/dependency_injector.dart
+++ lib/out-buildings/dependency_injector.dart
@@ -3,6 +3,9 @@ import 'package:get_it/get_it.dart';
 import 'package:nxt_test_case/core/extras/services/app-localization-service/app_localization_service.dart';
 import 'package:nxt_test_case/core/extras/services/app-navigation-service/app_navigation_service.dart';
 import 'package:nxt_test_case/core/extras/services/app-network-service/dio_network_service.dart';
+import 'package:nxt_test_case/core/layers/data/data-sources/reservations/reservations_remote_data_source.dart';
+import 'package:nxt_test_case/core/layers/data/repos/reservations/reservations_repo.dart';
+import 'package:nxt_test_case/core/layers/domain/usecases/reservations/reservations_usecase.dart';
 
 final serviceLocator = GetIt.instance;
 Future setupServiceLocator() async {
@@ -16,4 +19,8 @@ Future setupServiceLocator() async {
     serviceLocator.registerSingleton<Dio>(Dio(),);
     serviceLocator.registerLazySingleton<DioNetworkService>(() => DioNetworkService.create(serviceLocator<Dio>(),),);
   }
+  /////////////////////////
+  serviceLocator.registerFactory<ReservationsRemoteDataSource>(() => const ReservationsRemoteDataSource(),);
+  serviceLocator.registerFactory<ReservationsRepository>(() => ReservationsRepository(serviceLocator<ReservationsRemoteDataSource>(),),);
+  serviceLocator.registerFactory<FetchReservationsUsecase>(() => FetchReservationsUsecase(serviceLocator<ReservationsRepository>(),),);
 }
