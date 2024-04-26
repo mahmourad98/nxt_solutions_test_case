cc664873414942a4694671af86a871ef8c9afb22
mahmoudmourad <mahmoudmourad1998@gmail.com>
2024-04-26 23:36:16 +0300

    fix hotel reservations sub view to be a hook widget instead of stateful widget.

--- lib/core/layers/presentation/pages/reservation-details-page/sub-views/single_hotel_reservation_sub_view.dart
+++ lib/core/layers/presentation/pages/reservation-details-page/sub-views/single_hotel_reservation_sub_view.dart
@@ -1,39 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:nxt_test_case/core/layers/domain/entities/reservation/reservation.dart';
 import 'view-items/multiple_hotel_reservation_widget.dart';
 
-class HotelReservationsSubView extends StatefulWidget {
+class HotelReservationsSubView extends HookWidget {
   final Reservation reservation;
-  const HotelReservationsSubView(this.reservation,) : super(key: null,);
-  @override State<HotelReservationsSubView> createState() => _HotelReservationsSubViewState();
-}
-
-class _HotelReservationsSubViewState extends State<HotelReservationsSubView> {
-  bool get multipleHotelReservation => widget.reservation.hotelReservations.length > 1;
-  late final _keys = widget.reservation.hotelReservations.map((e) => ValueKey(e.hashCode,),).toList();
-  late final _valueNotifiers = widget.reservation.hotelReservations.map((e) => ValueNotifier(false,),).toList();
-  @override void initState() {
-    super.initState();
-    if (!multipleHotelReservation) {
-      _valueNotifiers.first.value = true;
-    }
-  }
+  late final _keys = reservation.hotelReservations.map((e) => ValueKey(e.hashCode,),).toList();
+  late final _valueNotifiers = reservation.hotelReservations.map((e) => ValueNotifier(false,),).toList();
+  /////////////////////////
+  HotelReservationsSubView(this.reservation,) : super(key: null,);
+  bool get multipleHotelReservation => reservation.hotelReservations.length > 1;
   @override Widget build(BuildContext context,) {
+    useEffect(() {
+      if (!multipleHotelReservation) {
+        _valueNotifiers.first.value = true;
+      }
+      return null;
+    }, const [],);
+    /////////////////////////
     return SliverList(
       delegate: SliverChildBuilderDelegate(
-        (_, int i,) {
+        (BuildContext buildContext, int i,) {
           return Padding(
             padding: const EdgeInsets.only(bottom: 8.0,),
             child: HotelReservationItemView(
               key: _keys[i],
               expansionValueNotifier: _valueNotifiers[i],
               isMultiple: multipleHotelReservation,
-              tickets: widget.reservation.tickets,
-              hotelReservation: widget.reservation.hotelReservations[i],
+              tickets: reservation.tickets,
+              hotelReservation: reservation.hotelReservations[i],
             ),
           );
         },
-        childCount: widget.reservation.hotelReservations.length,
+        childCount: reservation.hotelReservations.length,
       ),
     );
   }
